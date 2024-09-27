9461f123834d374638b6f1ef5b488b11314f1e04
MrZyzz23 <140208397+MrZyzz23@users.noreply.github.com>
2024-09-27 18:59:33 -0500

    social media agregadas

--- app/app.py
+++ app/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect
 app = Flask(__name__)
 
 # Lista para almacenar todas las transacciones
@@ -7,6 +7,12 @@ app = Flask(__name__)
 @app.route('/', methods=['POST', 'GET'])
 def index():
     return render_template('index.html')
+
+# Ruta para redirigir a WhatsApp
+@app.route('/whatsapp')
+def whatsapp():
+    whatsapp_url = "https://api.whatsapp.com/send?phone=573219043634&text=Hola"
+    return redirect(whatsapp_url)
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
